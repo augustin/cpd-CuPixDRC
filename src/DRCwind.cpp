9efd5659ab9f41073ab6142cea37fc5e0a0c1b36
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-01-07 20:19:29 -0500

    Add SelectDevice dialog.

--- src/DRCwind.cpp
+++ src/DRCwind.cpp
@@ -2,8 +2,7 @@
 #include "ui_DRCwind.h"
 
 #include "kernel/kernel.h"
-
-#include <QInputDialog>
+#include "dialogs/SelectDevice.h"
 
 DRCwind::DRCwind(QWidget *parent) :
     QMainWindow(parent),
@@ -14,15 +13,6 @@ DRCwind::DRCwind(QWidget *parent) :
 
     ui->actionOpen->setIcon(style()->standardIcon(QStyle::SP_DirOpenIcon));
     ui->actionRunDRC->setIcon(style()->standardIcon(QStyle::SP_CommandLink));
-
-    /* Create Device List */
-    int count = 0;
-    cudaGetDeviceCount(&count);
-    for(int i = 0; i < count; i++) {
-        cudaDeviceProp prop;
-        cudaGetDeviceProperties(&prop, i);
-        devices.append(prop.name);
-    }
 }
 
 DRCwind::~DRCwind()
@@ -37,6 +27,9 @@ void DRCwind::on_actionOpen_triggered()
 
 void DRCwind::on_actionRunDRC_triggered()
 {
-    int dev = devices.indexOf(QInputDialog::getItem(this, tr("Which device?"), tr("Execute DRC on:"), devices, 0, false));
-    kernel_main(dev);
+    SelectDevice d;
+    if(d.exec() == QDialog::Accepted) {
+        int dev = d.device();
+        kernel_main(dev);
+    }
 }
--- src/kernel/kernel.h
+++ src/kernel/kernel.h
@@ -1,9 +1,6 @@
 #ifndef KERNEL_MAIN_H
 #define KERNEL_MAIN_H
 
-#include <cuda.h>
-#include <cuda_runtime.h>
-
 void kernel_main(int device);
 
 #endif // KERNEL_MAIN_H
