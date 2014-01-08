7609b3f8d295ceee1125a9c1678e20e30e5b7862
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-01-07 20:24:05 -0500

    Launch in the center of the screen.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,12 @@
 #include <QApplication>
+#include <QDesktopWidget>
 #include "DRCwind.h"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
     DRCwind w;
+    w.move(app.desktop()->screen()->rect().center() - w.rect().center());
     w.show();
 
     return app.exec();
