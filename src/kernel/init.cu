75a9fcd9e3caa6c5eef736837841062b5a3b69d7
waddlesplash <ajcsweb@gmail.com>
2014-02-03 16:04:05 -0500

    Runs on the GPU! Only one core, though.

--- src/kernel/init.cu
+++ src/kernel/init.cu
@@ -1,44 +1,54 @@
-// This is the REAL "hello world" for CUDA!
-// It takes the string "Hello ", prints it, then passes it to CUDA with an array
-// of offsets. Then the offsets are added in parallel to produce the string "World!"
-// By Ingemar Ragnemalm 2010
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <memory.h>
-
 #include "init.h"
 #include "kernel.h"
+
+#include "errors.h"
 
 #ifdef CUDA
 int* kernel_main_cuda(int device)
 {
-    char a[N] = "Hello \0\0\0\0\0\0";
-    int b[N] = {15, 10, 6, 0, -11, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int* ret = (int*)malloc(sizeof(int)*3*MAX_ERRORS);
+    memset((char*)ret, '\0', sizeof(int)*3*MAX_ERRORS);
 
-    char *ad;
-    int *bd;
-    const int csize = N*sizeof(char);
-    const int isize = N*sizeof(int);
-
-    printf("%s", a);
-
-    cudaMalloc( (void**)&ad, csize );
-    cudaMalloc( (void**)&bd, isize );
-    cudaMemcpy( ad, a, csize, cudaMemcpyHostToDevice );
-    cudaMemcpy( bd, b, isize, cudaMemcpyHostToDevice );
+    int w = 10, h = 10;
+    char* pixels =
+        "xxxxxxxxxx"
+        "xxxxxxxxxx"
+        "xxxxxxxxxx"
+        "xxxxxxxxxx"
+        "xxxxx    x"
+        "xxxxxxxxxx"
+        "xxx xxxxxx"
+        "xxx xxxxxx"
+        "xxx xxxxxx"
+        "xxx xxxxxx";
 
     cudaSetDevice(device);
-    dim3 dimBlock( blocksize, 1 );
-    dim3 dimGrid( 1, 1 );
-	//device_drc<<<dimGrid, dimBlock>>>(*ad, 1, 1);
-    cudaMemcpy( a, ad, csize, cudaMemcpyDeviceToHost );
-    cudaFree( ad );
-    cudaFree( bd );
 
-    printf("%s\n", a);
+    char* devPixels;
+    int* error_buffer;
+    cudaMalloc((void**)&devPixels, w*h);
+    cudaMalloc((void**)&error_buffer, sizeof(int)*3*MAX_ERRORS);
+
+    cudaMemset(error_buffer, 0, sizeof(int)*3*MAX_ERRORS);
+
+    cudaMemcpy(devPixels, pixels, w*h, cudaMemcpyHostToDevice);
+
+    dim3 dimBlock(blocksize, 1);
+    dim3 dimGrid(1, 1);
+
+    device_drc<<<1, 1>>>(devPixels, w, h, error_buffer);
+
+    cudaMemcpy(ret, error_buffer, sizeof(int)*3*MAX_ERRORS, cudaMemcpyDeviceToHost);
+    cudaFree(error_buffer);
+    cudaFree(devPixels);
+
+    return ret;
 }
 #else
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
 int* kernel_main_cpu()
 {
 	int* ret = (int*)malloc(sizeof(int)*3*MAX_ERRORS);
