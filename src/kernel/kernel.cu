0fed26a3f173b0cc265cf462157e33903f1a089c
waddlesplash <ajcsweb@gmail.com>
2014-02-05 18:11:43 -0500

    WORKS ON THE GPU, IDENTICAL OUTPUT!

--- src/kernel/kernel.cu
+++ src/kernel/kernel.cu
@@ -3,6 +3,11 @@
 #include <limits.h>
 #include "errors.h"
 
+#define ERROR(id, x, y) error_buffer[errbuf] = id; \
+    error_buffer[errbuf+1] = x; \
+    error_buffer[errbuf+2] = y; \
+    errbuf += 3
+
 KERNEL_FUNCTION(void, drc) (char* pixels, int imgW, int imgH, int* error_buffer)
 {
     int totalThreads = THREADS_TOT, myThreadID = THREAD_ID;
@@ -11,60 +16,48 @@ KERNEL_FUNCTION(void, drc) (char* pixels, int imgW, int imgH, int* error_buffer)
     if(myThreadID > imgH || myThreadID > imgW)
         return;
 
-    int secW = imgW/totalThreads, secH = imgH/totalThreads;
-    if(secW == 0) { secW = 1; }
-    if(secH == 0) { secH = 1; }
-    int mySecX = secW*myThreadID, mySecY = secH*myThreadID;
-
     int errbuf = maxErrors*myThreadID;
-    error_buffer[errbuf] = I_THREAD_ID;
-    error_buffer[errbuf+1] = myThreadID;
-    error_buffer[errbuf+2] = THREADS_TOT;
-    error_buffer[errbuf+3] = I_SEC_DIM;
-    error_buffer[errbuf+4] = secW;
-    error_buffer[errbuf+5] = secH;
-    errbuf += 6;
+    ERROR(I_THREAD_ID, myThreadID, THREADS_TOT);
 
 #ifdef SMP
     SYNCTHREADS;
 #endif
 
-    int pixelsSinceBlack = INT_MAX;
-    for(int y = mySecY; y < mySecY+secH; y++) {
-		pixelsSinceBlack = INT_MAX;
-        for(int x = mySecX; x < mySecX+secW; x++) {
-			if(pixels[imgW*y+x] == 'x') {
-				if(pixelsSinceBlack < 4 && pixelsSinceBlack != 0) {
-					error_buffer[errbuf] = E_HOR_SPACING_TOO_SMALL;
-					error_buffer[errbuf+1] = x;
-					error_buffer[errbuf+2] = y;
-					errbuf += 3;
-				}
-				pixelsSinceBlack = 0;
-			} else {
-				pixelsSinceBlack++;
-			}
-		}
-	}
+    /* Horizontal check */
+    int y = myThreadID, pixelsSinceBlack;
+    while(y < imgH) {
+        pixelsSinceBlack = INT_MAX;
+        for(int x = 0; x < imgW; x++) {
+            if(pixels[imgW*y+x] == 'x') {
+                if(pixelsSinceBlack < 4 && pixelsSinceBlack != 0) {
+                    ERROR(E_HOR_SPACING_TOO_SMALL, x, y);
+                }
+                pixelsSinceBlack = 0;
+            } else {
+                pixelsSinceBlack++;
+            }
+        }
+        y += totalThreads;
+    }
 
 #ifdef SMP
     SYNCTHREADS;
 #endif
 
-    for(int x = mySecX; x < mySecX+secW; x++) {
-		pixelsSinceBlack = INT_MAX;
-        for(int y = mySecY; y < mySecY+secH; y++) {
-			if(pixels[imgW*y+x] == 'x') {
-				if(pixelsSinceBlack < 4 && pixelsSinceBlack != 0) {
-					error_buffer[errbuf] = E_VER_SPACING_TOO_SMALL;
-					error_buffer[errbuf+1] = x;
-					error_buffer[errbuf+2] = y;
-					errbuf += 3;
-				}
-				pixelsSinceBlack = 0;
-			} else {
-				pixelsSinceBlack++;
-			}
-		}
-	}
+    /* Vertical check */
+    int x = myThreadID;
+    while(x < imgW) {
+        pixelsSinceBlack = INT_MAX;
+        for(int y = 0; y < imgH; y++) {
+            if(pixels[imgW*y+x] == 'x') {
+                if(pixelsSinceBlack < 4 && pixelsSinceBlack != 0) {
+                    ERROR(E_VER_SPACING_TOO_SMALL, x, y);
+                }
+                pixelsSinceBlack = 0;
+            } else {
+                pixelsSinceBlack++;
+            }
+        }
+        x += totalThreads;
+    }
 }
