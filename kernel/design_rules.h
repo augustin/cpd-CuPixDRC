336ffe098cc0d0753ecaee307fa38f554ea26dab
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-02-28 18:15:41 -0500

    Modify design_rules.h to work with the CMF layer of the test chip.

--- kernel/design_rules.h
+++ kernel/design_rules.h
@@ -1,6 +1,12 @@
 #ifndef DESIGN_RULES_H
 #define DESIGN_RULES_H
 
-#define R_MIN_SPACE 4
+/* This is equal to 3 lambda at 1/14th scale,
+ * for the MIPS test chip. At 1/1 scale, that file
+ * has lambda=40.
+ *
+ * Note this only works for the 'CMF' layer.
+ */
+#define R_MIN_SPACE 8
 
 #endif // DESIGN_RULES_H
