5678c6cf2338ca3155aebd7c83aabde54538eef2
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-03-01 15:38:58 -0500

    Update design rules.

--- kernel/design_rules.h
+++ kernel/design_rules.h
@@ -1,12 +1,12 @@
 #ifndef DESIGN_RULES_H
 #define DESIGN_RULES_H
 
-/* This is equal to 3 lambda at 1/14th scale,
+/* This is equal to 3 lambda at 1/15th scale,
  * for the MIPS test chip. At 1/1 scale, that file
  * has lambda=40.
  *
  * Note this only works for the 'CMF' layer.
  */
-#define R_MIN_SPACE 8
+#define R_MIN_SPACE 6
 
 #endif // DESIGN_RULES_H
