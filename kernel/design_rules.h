5d3cfb0c66d0a8e250b5e3c99d763daba710f991
Augustin Cavalier <augustin.j.cavalier@gmail.com>
2014-03-03 15:35:43 -0500

    Fix min space.

--- kernel/design_rules.h
+++ kernel/design_rules.h
@@ -1,12 +1,6 @@
 #ifndef DESIGN_RULES_H
 #define DESIGN_RULES_H
 
-/* This is equal to 3 lambda at 1/15th scale,
- * for the MIPS test chip. At 1/1 scale, that file
- * has lambda=40.
- *
- * Note this only works for the 'CMF' layer.
- */
-#define R_MIN_SPACE 6
+#define R_MIN_SPACE 5
 
 #endif // DESIGN_RULES_H
